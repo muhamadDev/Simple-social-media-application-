58f9028ffd6cb60a65626c144aaa54d0d5deb068
Muhammad <127027527+muhamadDev@users.noreply.github.com>
2024-07-07 15:35:18 +0300

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+"use client"
+// loop throw an artay and return this component
+// and dont forget to replace loading... to a skeleton component
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from 'react';
+
+export default function Card({data}) {
+	const [isVisable, setIsVisable] = useState(false);
+	
+	const { ref, inView, entry } = useInView({
+		/* Optional options */
+		// triggerOnce: true,
+		rootMargin: "-250px",
+		threshold: 0,
+		
+	});
+
+	useEffect(() => { 
+		if (inView) {
+			setIsVisable(true)
+		} else {
+			setIsVisable(false)
+		}
+		
+	}, [inView])
+	
+	return (
+		<div ref={ref}>
+			{
+				isVisable ?
+				<div className="bg-blue-400 h-[70px] w-64">{data.body}</div> : 
+				<div>loading...</div>
+			}
+		</div>
+	)
+}
